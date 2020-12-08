a0d99b3414daae3294ec809364d988f98a6590d0
gautamgitspace <agautam2@buffalo.edu>
2020-12-08 14:36:18 -0500

    removing dict and running sum method

--- sliding_window/max_length_subarray_sum_equals_k.py
+++ sliding_window/max_length_subarray_sum_equals_k.py
@@ -1,31 +1,4 @@
 """
-same as subarray sum equals k. in that we used to
-return the number of such subarrays whose sum used
-to equal k.
-
-Here, we return the max len of one such subarray
-that sums up to k.
-
-clearly we need to play with index here. whenever
-d.get(sums - k) fulfills the criteria, we need to
-track the index. basically we need to track from
-where the stretch begins and until where it lasts
-
-PS - we discussed what a stretch is previously
-"""
-
-def max_sub_array_len_sum_equals_k(self, nums, k):
-    d = {0 : -1}
-    sums = max_len = 0
-    for i in range(len(nums)):
-        sums += nums[i]
-        if sums - k in d:
-            max_len = max(max_len, i - d[sums - k])
-        # store the index
-        d[sums] = i
-    return max_len
-
-"""
 Using sliding window:
 
 - condition given: sum should equal k (yields candidate answer)
